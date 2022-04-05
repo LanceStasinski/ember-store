febed01d2b290fe62b65ed3ae8995dac6afce488
LanceStasinski <lance.stasinski@gmail.com>
2022-04-05 10:04:42 -0400

    feat: Add shopping-cart service

--- app/components/product.js
+++ app/components/product.js
@@ -5,5 +5,7 @@ import { action } from '@ember/object';
 export default class ProductComponent extends Component {
   productImage = this.args.product.colors[0].image;
 
+  toggleZoom() {}
+
   alt = this.args.product.name;
 }
